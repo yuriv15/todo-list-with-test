b7366819cdb82f3a1d5c4b207e4486a234c6dfb7
Yuri Gonçalves <yuri_rjx@hotmail.com>
2022-12-26 21:01:12 -0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+        <q-header elevated>
+            <q-toolbar>
+                <q-toolbar-title class="row justify-center">
+                    Todo List
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script setup lang="ts"></script>
